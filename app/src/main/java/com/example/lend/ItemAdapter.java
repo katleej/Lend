b740f730b46ee07fc908759ee40bad5d6aef45c0
Aarushi Agrawal <aarushi.agrawal77@gmail.com>
2019-10-19 14:14:04 -0700

    fixed adapter + rv

---
+++ app/src/main/java/com/example/lend/ItemAdapter.java
@@ -0,0 +1,63 @@
+package com.example.lend;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.CustomViewHolder> {
+    Context context;
+    ArrayList<Item> items;
+
+    public ItemAdapter(Context context, ArrayList<Item> items) {
+        this.context = context;
+        this.items = items;
+
+    }
+
+    @NonNull
+    @Override
+    public ItemAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+        return new CustomViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemAdapter.CustomViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.tvLenderName.setText(item.getLender().getDisplayName());
+        holder.tvItemName.setText(item.getName());
+        holder.tvItemDesc.setText(item.getDesc());
+        Glide.with(context).load(item.photoURL()).into(holder.photo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        ImageView photo;
+        TextView tvLenderName;
+        TextView tvItemName;
+        TextView tvItemDesc;
+
+        public CustomViewHolder(View itemView) {
+            super(itemView);
+            photo = itemView.findViewById(R.id.ivPhoto);
+            tvLenderName = itemView.findViewById(R.id.tvLenderName);
+            tvItemName = itemView.findViewById(R.id.tvItemName);
+            tvItemDesc = itemView.findViewById(R.id.tvItemDesc);
+
+        }
+    }
+}
