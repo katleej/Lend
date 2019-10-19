44a0d1c7d0cf0cedbb1c9edd01b3f9783659f94e
Aarushi Agrawal <aarushi.agrawal77@gmail.com>
2019-10-19 11:27:46 -0700

    started item + booking class

---
+++ app/src/main/java/com/example/lend/Item.java
@@ -0,0 +1,12 @@
+package com.example.lend;
+
+import com.google.firebase.auth.FirebaseUser;
+
+public class Item {
+    public FirebaseUser lender;
+    public String itemName;
+    public String itemDescription;
+    public int price;
+    public boolean booked;
+
+}
