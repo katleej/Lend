f1339139356c99f937db6e426b08ed1279efec8e
Aarushi Agrawal <aarushi.agrawal77@gmail.com>
2019-10-19 17:30:20 -0700

    currently working on locations

--- app/src/main/java/com/example/lend/ListingsActivity.java
+++ app/src/main/java/com/example/lend/ListingsActivity.java
@@ -1,14 +1,75 @@
 package com.example.lend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ListingsActivity extends AppCompatActivity {
+    ArrayList<Item> items;
+    ItemAdapter adapter;
+    FloatingActionButton fabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listings);
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
+        Query ref = database.child("items");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                items.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    Item item = ds.getValue(Item.class);
+                    items.add(item);
+                }
+                setUpRV();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListingsActivity.this, AddItemActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void setUpRV() {
+        final RecyclerView recList = (RecyclerView) findViewById(R.id.recyclerView);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(ListingsActivity.this);
+        llm.setReverseLayout(true);
+        llm.setStackFromEnd(true);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+        adapter = new ItemAdapter(getApplicationContext(), items);
+        recList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        Log.d("XYZ", ((Integer) items.size()).toString());
     }
 }
