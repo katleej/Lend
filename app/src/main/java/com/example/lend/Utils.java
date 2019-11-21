884efa37aa591ba7d98996ad28979bc184997c63
Katniss Lee <jihyunlee@berkeley.edu>
2019-11-21 06:02:13 -0800

    item now also show's item name on firebase

--- app/src/main/java/com/example/lend/Utils.java
+++ app/src/main/java/com/example/lend/Utils.java
@@ -65,7 +65,7 @@ public final class Utils {
         item.put("Photo URL", photoURL);
         item.put("ID", item.hashCode());
         item.put("Booked" , murder);
-        db.collection("items").document(item.get("ID").toString())
+        db.collection("items").document(item.get("Item Name").toString())
                 .set(item)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
@@ -79,6 +79,20 @@ public final class Utils {
                         Log.w("Henlo", "Error writing document", e);
                     }
                 });
+//        db.collection("items").document(item.get("ID").toString())
+//                .set(item)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Log.d("Henlo", "DocumentSnapshot successfully written!");
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w("Henlo", "Error writing document", e);
+//                    }
+//                });
     }
 
     public static void bookingWrite(String itemID, String lenderID, String borrowerID, String active, String days)  {
