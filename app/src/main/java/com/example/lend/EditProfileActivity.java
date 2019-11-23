cd9b57ecf7e855a29f3cf8ef4c88a614aab78455
Katniss Lee <jihyunlee@berkeley.edu>
2019-11-23 05:24:47 -0800

    Aady try this

--- app/src/main/java/com/example/lend/EditProfileActivity.java
+++ app/src/main/java/com/example/lend/EditProfileActivity.java
@@ -3,54 +3,37 @@ package com.example.lend;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
-import android.graphics.Color;
-=======
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
->>>>>>> fa965827356abce5c65de406e677c7b4c4ea03ad
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-<<<<<<< HEAD
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-=======
-import android.view.View;
-import android.widget.Button;
->>>>>>> fa965827356abce5c65de406e677c7b4c4ea03ad
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import org.parceler.Parcels;
 
-<<<<<<< HEAD
+
 import java.util.HashMap;
 import java.util.Map;
-=======
+
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Calendar;
->>>>>>> fa965827356abce5c65de406e677c7b4c4ea03ad
+
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
