1646b6509a39f50225c52ed121b05e4149038afd
Matthew Lacayo <mattlacayo@berkeley.edu>
2020-03-07 01:00:54 -0800

    Added FirestorePod

---
+++ iOS/Lend/Lend/Model/CurrentUserData.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentUserData.swift
+//  Lend
+//
+//  Created by Matthew Lacayo on 3/7/20.
+//  Copyright © 2020 mdb. All rights reserved.
+//
+
+import Foundation
+
+class CurrentUserData {
+    var currentUser : LendUser!
+    
+    static func initializeUser() {
+        
+    }
+}
